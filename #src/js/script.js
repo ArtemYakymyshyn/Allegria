3fe0273c3c83101076880e057ecebe002122efc8
ArtemYakymyshyn <artem.yakymyshyn.2016@gmail.com>
2022-07-30 14:59:59 +0300

    adaptive and hover added

---
+++ #src/js/script.js
@@ -0,0 +1,17 @@
+const eyeColor = 'Green / Зелений';
+console.log(eyeColor);
+
+const burgerMenu = document.querySelector('.header__wrk__row__burgerwrapper');
+
+const burgerContent = document.querySelector('.header-categoriesAndLangComb');
+
+burgerMenu.addEventListener('click', (e) => {
+   burgerMenu.classList.toggle('header__wrk__row__burgerwrapper__activi');
+   burgerContent.classList.toggle('header-categoriesAndLangComb__activi');
+});
+
+const favouriteButton = document.querySelector('.header__row__fav');
+favouriteButton.addEventListener('click', (e) => {
+   favouriteButton.setAttribute('srcset', 'img/logos/favred.webp');
+   favouriteButton.setAttribute('src', 'img/logos/favred.webp')
+});
--- Allegria/js/script.js
+++ Allegria/js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var eyeColor = 'Green / Зелений';
+console.log(eyeColor);
+var burgerMenu = document.querySelector('.header__wrk__row__burgerwrapper');
+var burgerContent = document.querySelector('.header-categoriesAndLangComb');
+burgerMenu.addEventListener('click', function (e) {
+  burgerMenu.classList.toggle('header__wrk__row__burgerwrapper__activi');
+  burgerContent.classList.toggle('header-categoriesAndLangComb__activi');
+});
+var favouriteButton = document.querySelector('.header__row__fav');
+favouriteButton.addEventListener('click', function (e) {
+  favouriteButton.setAttribute('srcset', 'img/logos/favred.webp');
+  favouriteButton.setAttribute('src', 'img/logos/favred.webp');
+});
